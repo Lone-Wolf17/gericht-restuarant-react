984d57c8052b989f5c369941d7e42147dd186442
Korede <adeyanju.akorede1@gmail.com>
2023-08-05 22:28:39 +0100

    feat: intro video section

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 import Images from "./images";
-import meal from "../assets/meal.mp4";
+import MealMP4 from "../assets/meal.mp4";
 import Data from "./data";
 
-export { Images, meal, Data };
+export { Images, MealMP4, Data };
--- src/container/Intro/Intro.tsx
+++ src/container/Intro/Intro.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { MealMP4 } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef<HTMLVideoElement>(null);
+
+  return (
+    <div className="app__video">
+      <video ref={vidRef} src={MealMP4} controls={false} loop muted />
+
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={() => {
+            setPlayVideo(!playVideo);
+
+            if (vidRef.current) {
+              if (playVideo) {
+                vidRef.current.pause();
+              } else {
+                vidRef.current.play();
+              }
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
