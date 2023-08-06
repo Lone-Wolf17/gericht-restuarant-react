789ed4475d45a9455daa440616360999c626835a
Korede <adeyanju.akorede1@gmail.com>
2023-08-06 10:13:27 +0100

    feat: awards / laurels section

--- src/container/Laurels/Laurels.tsx
+++ src/container/Laurels/Laurels.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import './Laurels.css';
+import {
+  CormorantHeadText,
+  CormorantParagraph,
+  OpenSansParagraph,
+  SubHeading,
+} from "../../components";
+import { Images, Data } from "../../constants";
+import "./Laurels.css";
+import { Award } from "../../types";
 
-const Laurels = () => (
-  <div>
-    Laurels
+type AwardCardProps = {
+  award: Award;
+};
+const AwardCard = ({ award: { imgUrl, title, subtitle } }: AwardCardProps) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="award_image" />
+    <div className="app__laurels_awards-card_content">
+      <CormorantParagraph color="#DCCA8" text={title} />
+      <OpenSansParagraph text={subtitle} />
+    </div>
   </div>
 );
+
+const Laurels = () => <div className="app__bg app__wrapper section__padding" id="awards">
+  <div className="app__wrapper_info">
+    <SubHeading title="Awards & recognition"/>
+    <CormorantHeadText text="Our Laurels"/>
+    <div className="app__laurels_awards">
+      {Data.awards.map(award => <AwardCard award={award} key={award.title} />)}
+    </div>
+  </div>
+
+  <div className="app__wrapper_img">
+    <img src={Images.laurels} alt="laurels_img" />
+  </div>
+</div>;
 
 export default Laurels;
