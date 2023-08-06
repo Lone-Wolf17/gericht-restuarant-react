c7c0ea78170b9d514b7d908ad1cc57d82835629b
Korede <adeyanju.akorede1@gmail.com>
2023-08-06 20:40:34 +0100

    feat: footer

--- src/components/Footer/Newsletter.tsx
+++ src/components/Footer/Newsletter.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './Newsletter.css';
+import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
+import { OpenSansParagraph } from "../Text/Text";
+import CustomButton from "../CustomButton/CustomButton";
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
+      <OpenSansParagraph text="Subscribe To Our Newsletter" />
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Enter your email address" />
+      <CustomButton text="Subscribe" />
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React from "react";
+import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 
-import './Footer.css';
+import { FooterOverlay, Newsletter, OpenSansParagraph } from "../../components";
+import { Images } from "../../constants";
+import "./Footer.css";
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <OpenSansParagraph text="9 W 53rd St, New York, NY 10019, USA" />
+        <OpenSansParagraph text="+1 212-344-1230" />
+        <OpenSansParagraph text="1 212-555-1230" />
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={Images.gericht} alt="footer_logo" />
+        <OpenSansParagraph text='"The best way to find yourself is to lose yourself in the service of others."' />
+        <img src={Images.spoon} alt="spoon_img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Working Hours</h1>
+        <OpenSansParagraph text="Monday-Friday:" />
+        <OpenSansParagraph text="08:00 am - 12:00 am" />
+        <OpenSansParagraph text="Saturday-Sunday:" />
+        <OpenSansParagraph text="07:00 am - 11:00 pm" />
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <OpenSansParagraph text="2021 Gericht. All Rights reserved." />
+    </div>
   </div>
 );
 
